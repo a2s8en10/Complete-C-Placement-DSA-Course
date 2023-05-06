05d858e0edc8b72fccb696a11fc6bab745017ce0
Anurag Sahu <anu.sahu9589@gmail.com>
2023-05-06 14:37:47 +0530

    update

---
+++ Pattern/while/Matrics_pattren_3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Enter the number :";  
+	cin >> n;
+	
+	int i = 1;
+	while(i<=n)
+	{
+		int j = 1;
+		while(j<=n)
+		{	
+		// i or j ko isliye ek sath liya hai
+		// kuyki row or colomn ek sth incriment ho rahe hai 
+			char ch = 'A'+ i + j - 2 ;
+			cout << ch << " ";
+			j = j + 1;
+		} 
+		cout << endl;
+		i = i + 1;
+	}
+}
