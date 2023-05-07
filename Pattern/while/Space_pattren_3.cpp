ff0986ad00f177d1c63df9d2110c7fc5eb2c0d42
Anurag Sahu <anu.sahu9589@gmail.com>
2023-05-08 00:19:29 +0530

    update

---
+++ Pattern/while/Space_pattren_3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Enter the number :";  
+	cin >> n;
+	
+	int i = 1 ;//row
+	while(i<=n)
+	{
+		int j = i; // col
+		while(j<=n)
+		{	
+			cout << "*";
+			j = j + 1;
+		}
+		cout << endl;
+		i = i + 1;		
+	}	
+}
---
+++ Pattern/while/Space_pattren_4.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Enter the number :";  
+	cin >> n;
+	
+	int i = 1 ;//row
+	while(i<=n)
+	{
+		int j = 1; // col
+		while(j<=i)
+		{	
+			int value;
+			cout << "*";
+			j = j + 1;
+		}
+		cout << endl;
+		i = i + 2;
+	}
+}
