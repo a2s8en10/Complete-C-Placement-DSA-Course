58632e4867ea31c170a48793e7b88ff83a081258
Anurag Sahu <anu.sahu9589@gmail.com>
2023-06-16 00:17:10 +0530

    update

---
+++ switch statement/How many note.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std ;
+int main ()
+{
+	int n, sum;
+	
+	cin >> n;
+	switch (n)
+	{
+		case 100 : 
+			
+			while (n<=100){
+				sum=n/100;
+				cout << sum ;				
+			}
+			break;
+			
+		case 50 : 
+		
+		case 20 :
+		
+		case 1 : 
+		
+		
+	}
+	
+}
