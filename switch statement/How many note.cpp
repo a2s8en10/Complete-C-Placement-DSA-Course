bb993e97f65332b676fb7744dc989402b534ad65
Anurag Sahu <anu.sahu9589@gmail.com>
2023-06-17 22:54:11 +0530

    update

--- switch statement/How many note.cpp
+++ switch statement/How many note.cpp
@@ -4,6 +4,7 @@ int main ()
 {
 	int n, sum;
 	
+	cout << "Enter the number: ";
 	cin >> n;
 	switch (n)
 	{
@@ -11,17 +12,38 @@ int main ()
 			
 			while (n<=100){
 				sum=n/100;
-				cout << sum ;				
+				cout << sum ;
+				n=n+1;				
 			}
 			break;
 			
 		case 50 : 
+			
+			while (sum<=50){
+				sum=sum/50;
+				cout << sum;
+				sum=sum+1;
+			}
+			break;
 		
 		case 20 :
+			
+			while (sum<=20){
+				sum=sum/20;
+				cout << sum;
+				sum=sum+1;
+			}
+			break;
 		
 		case 1 : 
 		
-		
+			while (sum<=1){
+				sum=sum/1;
+				cout << sum;
+				sum=sum+1;
+			}
+			break;
+			
 	}
 	
 }
