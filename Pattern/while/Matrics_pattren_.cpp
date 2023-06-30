8c77196171fbbcd6a46add20ae84dda7fb066da4
Anurag Sahu <anu.sahu9589@gmail.com>
2023-06-30 13:42:52 +0530

    update

--- Pattern/while/Matrics_pattren_.cpp
+++ Pattern/while/Matrics_pattren_.cpp
@@ -3,19 +3,25 @@ using namespace std;
 int main()
 {
 	int n;
+	
 	cout << "Enter the number :";  
 	cin >> n;
-	// i ko hum row or j ko coloumn mana hai
+	
+	// i ko hum row or j ko coloumn mane hai
 	int i = 1;
+	
 	while(i<=n)
 	{
 		int j = 1;
+		
 		while(j<=n)
+		
 		{	// A is a character data type 
 			char ch = 'A'+ i - 1 ;
 			cout << ch << " ";
 			j = j + 1;
 		} 
+		
 		//cout use in a new line
 		cout << endl;
 		i = i + 1;
