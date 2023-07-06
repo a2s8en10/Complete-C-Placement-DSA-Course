9e375238c86e8e66df8b4617ac316e55eac5c802
Anurag Sahu <anu.sahu9589@gmail.com>
2023-07-06 22:56:07 +0530

    update

--- Operators/Continue.cpp
+++ Operators/Continue.cpp
@@ -2,15 +2,23 @@
 using namespace std;
 int main(){
 	
-	for (int i= 0; i<5; i++)
+	int n;
+	
+	cout << "enter the number : " ;
+	cin >> n;
+	
+	for (int i= 0; i<n; i++)
 	{
 		cout << "Hii" << endl ;
+		
 		cout << "Heyyy" << endl;
+		
 		cout << endl;
 		
 		continue;
 		
 		cout << endl;
+		
 		cout << "Hello";
 	}
 }
